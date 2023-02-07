d10aec5e989ac5221f9f9029c5a06e92f1d19a35
Tanmayboy <100485745+Tanmayboy@users.noreply.github.com>
2023-02-07 12:24:12 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,7 @@
+const movingText = document.getElementById('moving-text');
+
+// Change the text
+movingText.innerHTML = 'New text';
+
+// Change the duration of the animation
+movingText.style.animationDuration = '10s';
